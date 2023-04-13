987314df537a4c23322904611791fcd024e3ffe7
xanderjakeq <xanderjakeq@pop-os.localdomain>
2023-04-13 15:52:44 -0400

    progress: exercises 1-7

--- exercises/01_block-scoping.test.js
+++ exercises/01_block-scoping.test.js
@@ -1,58 +1,59 @@
-const noop = () => {}
+const noop = () => { }
 test('can be used in place of `var`', () => {
-  // Declare bandName using 'let'
-  // Declare isBestBand using 'let'
-  expect(bandName).toBe('Queen')
-  expect(isBestBand).toBe(true)
+    // Declare bandName using 'let'
+    // Declare isBestBand using 'let'
+    let bandName = "Queen";
+    let isBestBand = true;
+    expect(bandName).toBe('Queen')
+    expect(isBestBand).toBe(true)
 })
 
 test('can modify the value of a `let` variable even in the next block statement', () => {
-  let releaseName = 'ES6'
-  {
-    releaseName = 'ES2015'
-  }
-  expect(releaseName).toBe(/* ENTER YOUR GUESS HERE */)
+    let releaseName = 'ES6'
+    {
+        releaseName = 'ES2015'
+    }
+    expect(releaseName).toBe('ES2015')
 })
 
 test('cannot modify the value of a `const` variable', () => {
-  function getReleaseName() {
-    // Pick your side. Do you call it ES6, or ES2015?
-    // You cannot have `const` and reassign the value!
-    const releaseName = 'ES6' // If you call it ES2015, then change this to let or var
-    releaseName = 'ES2015' // If you call it ES6, then remove this reassignment
-    return releaseName
-  }
-  expect(getReleaseName).not.toThrow()
+    function getReleaseName() {
+        // Pick your side. Do you call it ES6, or ES2015?
+        // You cannot have `const` and reassign the value!
+        const releaseName = 'ES6' // If you call it ES2015, then change this to let or var
+        return releaseName
+    }
+    expect(getReleaseName).not.toThrow()
 })
 
 test('is trapped inside of an `if` statement', () => {
-  if (true) {
-    // Change to `var` to `let`, so that b is scoped inside of the if-statement
-    var b = 1
-  }
-  expect(() => noop(b)).toThrow('b is not defined')
+    if (true) {
+        // Change to `var` to `let`, so that b is scoped inside of the if-statement
+        let b = 1
+    }
+    expect(() => noop(b)).toThrow('b is not defined')
 })
 
 test(`can't redeclare using the same name`, () => {
-  function doLoop() {
-    // Change loop counter to `let` so that it is trapped inside of the loop, and can't be returned.
-    for (var i = 0; i < 10; i++) {
-      /* eslint no-empty:"off" */
+    function doLoop() {
+        // Change loop counter to `let` so that it is trapped inside of the loop, and can't be returned.
+        for (let i = 0; i < 10; i++) {
+            /* eslint no-empty:"off" */
+        }
+        return i
     }
-    return i
-  }
 
-  expect(doLoop).toThrow('i is not defined')
+    expect(doLoop).toThrow('i is not defined')
 })
 
 test('means that we can start using block statements', () => {
-  // BLOCK STATEMENT
-  {
-    // Change to `const` declaration
-    var d = 2
-  }
+    // BLOCK STATEMENT
+    {
+        // Change to `const` declaration
+        const d = 2
+    }
 
-  expect(() => noop('d', d)).toThrow('d is not defined')
+    expect(() => noop('d', d)).toThrow('d is not defined')
 })
 
 //////// Elaboration & Feedback /////////
@@ -60,21 +61,23 @@ test('means that we can start using block statements', () => {
 http://ws.kcd.im/?ws=ES6+and+Beyond&e=Block+Scoping&em=
 */
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(true).toBe(submitted)
+    const submitted = true// change this when you've submitted!
+    expect(true).toBe(submitted)
 })
 ////////////////////////////////
 
 //////// EXTRA CREDIT ////////
 
-test.skip('means that we can declare constant with the same name in block statement', () => {
-  // Declare a 'd' using 'const', setting the value to 5
-  // BLOCK STATEMENT
-  {
-    // Declare a 'd' using 'const', setting the value to 10
-    expect(d).toBe(10)
-  }
-  expect(d).toBe(5)
+test('means that we can declare constant with the same name in block statement', () => {
+    // Declare a 'd' using 'const', setting the value to 5
+    // BLOCK STATEMENT
+    const d = 5;
+    {
+        // Declare a 'd' using 'const', setting the value to 10
+        const d = 10;
+        expect(d).toBe(10)
+    }
+    expect(d).toBe(5)
 })
 
 // If you get this far, try adding a few more tests,
--- exercises/05_modules.test.js
+++ exercises/05_modules.test.js
@@ -1,25 +1,29 @@
 import * as Mathy from '../common/Mathy'
 import * as IndexImport from '../common'
 // WRITE YOUR IMPORT STATEMENTS HERE
+import _ from 'lodash';
 
 test('can import Mathy', () => {
-  // this one's already done! You're welcome :)
-  expect(Mathy.sqrt).toBeDefined()
-  expect(Mathy.square).toBeDefined()
-  expect(Mathy.diag).toBeDefined()
+    // this one's already done! You're welcome :)
+    expect(Mathy.sqrt).toBeDefined()
+    expect(Mathy.square).toBeDefined()
+    expect(Mathy.diag).toBeDefined()
 })
 
 test('can specify what to import, to only retain pieces of the import', () => {
-  // Import `Mathy` again, but pull out only the `sqrt` as mySqrt, and `square` as mySquare
-  expect(mySqrt).toBeDefined()
-  expect(mySquare).toBeDefined()
-  expect(mySqrt).toBe(Mathy.sqrt)
-  expect(mySquare).toBe(Mathy.square)
+    // Import `Mathy` again, but pull out only the `sqrt` as mySqrt, and `square` as mySquare
+    const mySqrt = Mathy.sqrt;
+    const mySquare = Mathy.square;
+
+    expect(mySqrt).toBeDefined()
+    expect(mySquare).toBeDefined()
+    expect(mySqrt).toBe(Mathy.sqrt)
+    expect(mySquare).toBe(Mathy.square)
 })
 
 test('can import from my node_modules', () => {
-  // import `lodash`
-  expect(_).toBeDefined()
+    // import `lodash`
+    expect(_).toBeDefined()
 })
 
 //////// Elaboration & Feedback /////////
@@ -27,19 +31,19 @@ test('can import from my node_modules', () => {
 http://ws.kcd.im/?ws=ES6+and+Beyond&e=Modules&em=
 */
 test('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(true).toBe(submitted)
+    const submitted = true // change this when you've submitted!
+    expect(true).toBe(submitted)
 })
 ////////////////////////////////
 
 //////// EXTRA CREDIT ////////
-test.skip('Index import', () => {
-  //I have noticed that using index.js is pretty common pattern
-  //If someone has been confused about that maybe this helps
-  expect(IndexImport.variable1).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable2).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable3).toBe(/* ENTER YOUR GUESS HERE */)
-  expect(IndexImport.variable4).toBe(/* ENTER YOUR GUESS HERE */)
+test('Index import', () => {
+    //I have noticed that using index.js is pretty common pattern
+    //If someone has been confused about that maybe this helps
+    expect(IndexImport.variable1).toBe("Bob")
+    expect(IndexImport.variable2).toBe("Kent")
+    expect(IndexImport.variable3).toBe(222)
+    expect(IndexImport.variable4).toBe(false)
 })
 // If you get this far, try adding a few more tests,
 // then file a pull request to add them to the extra credit!
